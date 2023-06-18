1cba9f5c9ddf111556e500e914958b2a8f5723c0
Swenydream <118567249+Swenydream@users.noreply.github.com>
2023-06-18 13:18:23 -0400

    Add files via upload

---
+++ my work/js/display.js
@@ -0,0 +1,48 @@
+var currentdisplayNo = 0;
+var display1 = document.getElementById('display-1');
+var display2 = document.getElementById('display-2');
+var display3 = document.getElementById('display-3');
+var displayO = document.getElementById('display-O');
+var displayC = document.getElementById('display-C');
+
+//elevator
+{
+
+    //when 2 is pressed
+    {
+        //if floor is at 3 down indicator glows
+
+        //if floor is at 1 up indicator glows
+    }
+      //when 3 is pressed
+     {
+            //if floor is at 2 up indicator glows
+    
+            //if floor is at 1 up indicator glows
+     }
+
+      //when 1 is pressed
+      {
+        //if floor is at 3 down indicator glows
+
+        //if floor is at 2 down indicator glows
+    }
+
+}
+
+//door
+{
+    //when door open
+    {
+        //Open indicator shows
+
+    }
+
+    //when door closes
+    {
+        //Close indicator shows
+
+    }
+
+
+}
